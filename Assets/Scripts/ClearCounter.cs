b838c4bddc29d20d03b645bf2d8f01fa141d67ff
JesseAdams313 <echodevgames@gmail.com>
2026-02-19 15:46:48 -0500

    Initialize the GIT

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,12 @@
+//-----ClearCounter.cs START-----
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Interacting with a ClearCounter");
+
+    }
+}
+//-----ClearCounter.cs END-----
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,21 @@
+//-----PlayerAnimator.cs START-----
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private PlayerController playerController;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, playerController.IsWalking());
+    }
+}
+//-----PlayerAnimator.cs END-----
