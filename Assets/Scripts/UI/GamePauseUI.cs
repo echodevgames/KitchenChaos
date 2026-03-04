8930bf89f1abdc47d59018f055feb72035f35871
JesseAdams313 <echodevgames@gmail.com>
2026-03-03 20:08:01 -0500

    Lecture 40. Options, Audio Levels

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -6,6 +6,7 @@ public class GamePauseUI : MonoBehaviour
 {
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
 
     private void Awake()
     {
@@ -15,7 +16,11 @@ public class GamePauseUI : MonoBehaviour
         });
         mainMenuButton.onClick.AddListener(() =>
         {
-            Loader.Load (Loader.Scene.MainMenuScene);
+            Loader.Load(Loader.Scene.MainMenuScene);
+        }); 
+        optionsButton.onClick.AddListener(() =>
+        {
+            OptionsUI.Instance.Show();
         });
     }
 
