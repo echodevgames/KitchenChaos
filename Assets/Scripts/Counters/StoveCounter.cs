61d0770969ef3ef8ba083d82539adc8a4efb014c
JesseAdams313 <echodevgames@gmail.com>
2026-03-04 20:44:20 -0500

    43. Polish 0.6 StoveBurningBarFlash

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -226,4 +226,12 @@ public class StoveCounter : BaseCounter, IHasProgress
         }
         return null;
     }
+
+
+    public bool IsFried()
+    {
+        return state == State.Fried;
+    }
+
+
 }
--- Assets/Scripts/Counters/StoveCounterSound.cs
+++ Assets/Scripts/Counters/StoveCounterSound.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class StoveCounterSound : MonoBehaviour
 {
-private AudioSource audioSource;
-
     [SerializeField] private StoveCounter stoveCounter;
+
+    private AudioSource audioSource;
+    private float warningSoundTimer;
+    private bool playWarningSound;
+
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
@@ -13,6 +16,15 @@ private AudioSource audioSource;
     private void Start()
     {
         stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+        stoveCounter.OnProgressChanged += StoveCounter_OnProgressChanged;
+    }
+
+    private void StoveCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
+    {
+
+        float burnShowProgressAmount = .5f;
+        playWarningSound = stoveCounter.IsFried() && e.progressNormalized >= burnShowProgressAmount;
+
     }
 
     private void StoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
@@ -26,5 +38,21 @@ private AudioSource audioSource;
         {
             audioSource.Pause();
         }
+
     }
+    private void Update()
+    {
+        if(playWarningSound) {
+        warningSoundTimer -= Time.deltaTime;
+            if (warningSoundTimer <= 0f)
+            {
+                float warningSoundTimeMax = .2f;
+                warningSoundTimer = warningSoundTimeMax;
+
+                SoundManager.Instance.PlayWarningSound(stoveCounter.transform.position);
+            }
+        }
+    }
+
+
 }
