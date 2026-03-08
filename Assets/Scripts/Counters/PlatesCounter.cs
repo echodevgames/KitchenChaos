6c0969bca547ef67c7b3397d723797e0f2033d10
JesseAdams313 <echodevgames@gmail.com>
2026-03-07 21:11:33 -0500

    Lecture 8. Sync Plates Counter

--- Assets/Scripts/Counters/PlatesCounter.cs
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PlatesCounter : BaseCounter
@@ -15,6 +16,10 @@ public class PlatesCounter : BaseCounter
 
     private void Update()
     {
+        if(!IsServer)
+        {
+            return; 
+        }
         spawnPlateTimer += Time.deltaTime;
         if (spawnPlateTimer > spawnPlateTimerMax)
         {
@@ -22,13 +27,24 @@ public class PlatesCounter : BaseCounter
 
             if (KitchenGameManager.Instance.IsGamePlaying() && platesSpawnedAmount < platesSpawnedAmountMax)
             {
-                platesSpawnedAmount++;
-
-                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+                SpawnPlateServerRpc();
             }
         }
     }
+    [ServerRpc]
+    private void SpawnPlateServerRpc()
+    {
+        SpawnPlateClientRpc();
+    }
 
+    [ClientRpc]
+
+    private void SpawnPlateClientRpc()
+    {
+        platesSpawnedAmount++;
+
+        OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+    }
     public override void Interact(PlayerController playerController)
     {
         if (!playerController.HasKitchenObject())
@@ -36,12 +52,26 @@ public class PlatesCounter : BaseCounter
             if (platesSpawnedAmount > 0)
             {
                 //theres at least one plate here
-                platesSpawnedAmount--;
 
                 KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, playerController);
-
-                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+                InteractLogicServerRpc();
             }
         }
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLogicServerRpc()
+    {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractLogicClientRpc()
+    {
+
+        platesSpawnedAmount--;
+        OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+    }
+
+
 }
